b03402ccaf0f82ac520adfea43bd476f6b85ff88
Gerard Gascón <dev@gerardgascon.com>
2026-04-21 11:30:14 +0200

    pasdiofhj

---
+++ Tests/FizzBuzz.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests;
+
+public static class FizzBuzz {
+	public static string FizzBuzzConverter(int i)
+	{
+		if (i % 3 == 0)
+		{
+			return "Fizz";
+		}
+		return i.ToString();
+	}
+}
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -3,33 +3,24 @@ namespace Tests;
 public class Tests {
 	[Test]
 	public void OneReturnsOne() {
-		Assert.AreEqual(FizzBuzzConverter(1), "1");
+		Assert.AreEqual(FizzBuzz.FizzBuzzConverter(1), "1");
 	}
 
 	[Test]
 	public void TwoReturnsTwo()
 	{
-		Assert.AreEqual(FizzBuzzConverter(2), "2");
+		Assert.AreEqual(FizzBuzz.FizzBuzzConverter(2), "2");
 	}
 
 	[Test]
 	public void ThreeReturnsFizz()
 	{
-		Assert.AreEqual(FizzBuzzConverter(3), "Fizz");
+		Assert.AreEqual(FizzBuzz.FizzBuzzConverter(3), "Fizz");
 	}
 
 	[Test]
 	public void SixReturnsFizz()
 	{
-		Assert.AreEqual(FizzBuzzConverter(6), "Fizz");
-	}
-
-	private string FizzBuzzConverter(int i)
-	{
-		if (i % 3 == 0)
-		{
-			return "Fizz";
-		}
-		return i.ToString();
+		Assert.AreEqual(FizzBuzz.FizzBuzzConverter(6), "Fizz");
 	}
 }
