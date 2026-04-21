93d29753e8112c1035c65cd5714211926e9512ca
Gerard Gascón <dev@gerardgascon.com>
2026-04-21 11:27:30 +0200

    aopsdifh

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -3,28 +3,28 @@ namespace Tests;
 public class Tests {
 	[Test]
 	public void OneReturnsOne() {
-		Assert.AreEqual(asdoifuh(1), "1");
+		Assert.AreEqual(FizzBuzzConverter(1), "1");
 	}
 
 	[Test]
 	public void TwoReturnsTwo()
 	{
-		Assert.AreEqual(asdoifuh(2), "2");
+		Assert.AreEqual(FizzBuzzConverter(2), "2");
 	}
 
 	[Test]
 	public void ThreeReturnsFizz()
 	{
-		Assert.AreEqual(asdoifuh(3), "Fizz");
+		Assert.AreEqual(FizzBuzzConverter(3), "Fizz");
 	}
 
 	[Test]
 	public void SixReturnsFizz()
 	{
-		Assert.AreEqual(asdoifuh(6), "Fizz");
+		Assert.AreEqual(FizzBuzzConverter(6), "Fizz");
 	}
 
-	private string asdoifuh(int i)
+	private string FizzBuzzConverter(int i)
 	{
 		if (i == 3 || i == 6)
 		{
